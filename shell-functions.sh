10b4bef135ada21a59c77f7122500e812f76fa48
ldelossa <louis.delos@gmail.com>
2023-02-23 09:34:25 -0500

    improve shell functions, quite container start, iproute-tc in home
    Signed-off-by: ldelossa <louis.delos@gmail.com>

--- shell-functions.sh
+++ shell-functions.sh
@@ -3,11 +3,46 @@
 p="$HOME/git/devspaces"
 
 function devspace() {
+    if [[ "$1" = "list" ]]; then
+        devspace-list
+        return
+    fi
+
+    if [[ "$1" = "stop" ]]; then
+        devspace-stop "$2"
+        return
+    fi
+
+    if [[ "$1" = "rm" ]]; then
+        devspace-rm "$2"
+        return
+    fi
+
+    if [[ -z "$1" ]]; then
+        echo "
+devspace list
+devspace stop
+devspace rm
+devspace {dev container name}
+        "
+        return
+    fi
+
+
     cd "$p"/"$1" || return
     make run
 }
 
 function devspace-list() {
-    docker ps | head -n 1
-    docker ps | grep "devspace/"
+    docker ps -a --filter=label=devspace --format "table {{.Names}}\t{{.Status}}"
 }
+
+function devspace-stop() {
+    docker stop "$1"
+}
+
+function devspace-rm() {
+    docker rm "$1"
+}
+
+
